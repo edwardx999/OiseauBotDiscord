13e2535e34e059889b1ff6446ac1f2018eaa332c
The_Number_e <edwardx999@gmail.com>
2020-12-01 22:58:07 -0500

    fix uncaught spawn errors

--- file_util.ts
+++ file_util.ts
@@ -22,25 +22,29 @@ interface SpawnResult {
 
 
 const spawnTimeout = (command: string, args: string[], timeoutMs: number, options?: cp.SpawnOptions) => {
-	return new Promise<SpawnResult>((resolve) => {
-		const child = cp.spawn(command, args, options);
-		let stdout = "";
-		let stderr = "";
-		let timedOut = false;
-		const timeout = setTimeout(() => {
-			child.kill();
-			timedOut = true;
-		}, timeoutMs);
-		child.stdout.on("data", data => stdout += data);
-		child.stderr.on("data", data => stderr += data);
-		child.on("close", code => {
-			if (!timedOut) {
-				resolve({ stdout, stderr, exitCode: code });
-				clearTimeout(timeout);
-			}
-			else {
-				resolve({ stdout, stderr });
-			}
-		});
+	return new Promise<SpawnResult>((resolve, reject) => {
+		try {
+			const child = cp.spawn(command, args, options);
+			let stdout = "";
+			let stderr = "";
+			let timedOut = false;
+			const timeout = setTimeout(() => {
+				child.kill();
+				timedOut = true;
+			}, timeoutMs);
+			child.stdout.on("data", data => stdout += data);
+			child.stderr.on("data", data => stderr += data);
+			child.on("close", code => {
+				if (!timedOut) {
+					resolve({ stdout, stderr, exitCode: code });
+					clearTimeout(timeout);
+				}
+				else {
+					resolve({ stdout, stderr });
+				}
+			});
+		} catch (err) {
+			reject(err);
+		}
 	});
 };
